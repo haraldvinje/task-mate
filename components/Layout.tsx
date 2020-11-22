7927d15980cbc0545276c575114fd4c5182fd4f3
Harald Vinje <haraldvinje@gmail.com>
2020-11-22 22:43:44 +0100

    changed logo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 interface Props {}
 
-const Layout: React.FC<Props> = ({ children }) => {
+const Layout: React.FC<Props> = ({children}) => {
   return (
     <div className="page">
       <Link href="/">
         <a className="logo">
-          <img src="/logo.png" alt="logo" />
+          <img src="/todologo.png" alt="logo" />
         </a>
       </Link>
       {children}
